4fc333b988aef006f28591cab2aeab34f1d369d6
Laura Laugwitz <s0544049@htw-berlin.de>
2015-11-25 21:28:12 +0100

    Refactor

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Question, type: :model do
     expect(Question.new().valid?).to eq(false)
   end
   it "has a unique URL" do
-    Question.create(title: "Unicorn Rainbow?", url: "jhgl")
+    create(:question, url: "jhgl")
     expect(Question.create(title: "Doughnut Rainbow?",url: "jhgl").valid?).to be(false)
   end
   it "is initialized with a url" do
-    question = Question.create(title: "Are you Pinkie Pie?")
+    question = create(:question)
     expect(question.url).to_not be nil
   end
 end
