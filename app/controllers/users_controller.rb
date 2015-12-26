12b11a05303e2ed98d9dfd13115933f2ed580d81
Laura Laugwitz <s0544049@htw-berlin.de>
2015-12-26 15:31:38 +0100

    Allow delete user for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:index, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -12,6 +13,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
   end
 
   def create
@@ -57,6 +64,11 @@ class UsersController < ApplicationController
    def correct_user
      @user = User.find(params[:id])
      redirect_to(root_url) unless current_user?(@user)
+   end
+
+   # Confirms Admin User
+   def admin_user
+     redirect_to(root_url) unless current_user.admin?
    end
 
 end
