476d6a0ae78b5db16db3ba87e049a03673da307d
Laura Laugwitz <laura.laugwitz@hitfoxgroup.com>
2016-01-18 14:00:15 +0100

    Adjust configs for paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,14 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :avatar,
+                    escape_url: false,
+                    styles: {
+                      thumb: '100x100>',
+                      square: '200x200#',
+                      medium: '300x300>'
+                    }
+
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Paperclip & S3 Configs
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region => 'frankfurt'
+  :s3_region => 'eu-central-1',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
